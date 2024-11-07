cd7a2057682e1c03d0976561a7d2216ec1d973c1
bruno-santos382 <bruno.santos382@fatec.sp.gov.br>
2024-11-07 20:25:42 -0300

    Corrigir base href das páginas

--- src/template/admin/header.php
+++ src/template/admin/header.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="<?= htmlentities($config['app.url']) ?>">
+    <base href="<?= $config['app.url'].'/' ?>">
     <title><?= $template['titulo'] ?></title>
 
     <?php if (!empty($template['styles'])): ?>

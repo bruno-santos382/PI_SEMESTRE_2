f49f4d9df0b264597264b8b8cb597643172c3587
bruno-santos382 <bruno.santos382@fatec.sp.gov.br>
2024-11-12 19:16:11 -0300

    Verificar permissão ao acessar páginas

--- src/template/admin/header.php
+++ src/template/admin/header.php
@@ -1,16 +1,32 @@
-<?php $config = include '../src/includes/config.php'; ?>
+<?php 
+$config = include __DIR__.'/../../includes/config.php';
+
+ // Verificar se usuario está logado, a menos que seja desabilitado propositalmente
+if (!isset($template['verifica_login']) || $template['verifica_login'] === true)
+{
+    require_once __DIR__.'/../../class/autenticacao/Autentica.php';
+
+    $autentica = new Autentica();
+    
+    if (!$autentica->usuarioLogado()) {
+        header('Location: ' . $config['app.url'] . '/admin/login.php');
+        exit;
+    }    
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="<?= $config['app.url'].'/' ?>">
-    <title><?= $template['titulo'] ?></title>
+    <base href="<?= htmlspecialchars($config['app.url'].'/') ?>">
+    <title><?= htmlspecialchars($template['titulo']) ?></title>
 
     <?php if (!empty($template['styles'])): ?>
         <?php foreach ($template['styles'] as $href): ?>
-            <link rel="stylesheet" href="<?= $href ?>">
+            <link rel="stylesheet" href="<?= htmlspecialchars($href) ?>">
         <?php endforeach; ?>
     <?php endif; ?>
 </head>
